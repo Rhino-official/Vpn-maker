02bbdc968b7c55d8bf9ad09e1d5567cdbcbaf2b2
Rhino-official <103337249+Rhino-official@users.noreply.github.com>
2022-05-02 08:57:56 +0530

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,7 @@
+#Create a list of Free VPN server codes provided by Windscribe (VPN).
+
+ 
+
+# list of VPN server codes
+codeList = ["TR", "US-C", "US", "US-W", "CA", "CA-W",
+            "FR", "DE", "NL", "NO", "RO", "CH", "GB", "HK"]
