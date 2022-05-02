21aff2bbef44d84f2e87f0d628db2e8a69e091cb
Rhino-official <103337249+Rhino-official@users.noreply.github.com>
2022-05-02 08:57:08 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+//////////////Open your terminal (Ctrl+Alt+T) and create a file using gedit by typing the following command on the terminal.////////////////////////////
+////////////////////gedit gfg.py//////////////////////
+import os
+from time import sleep
+import random
